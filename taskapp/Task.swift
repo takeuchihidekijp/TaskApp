e44909e530d023f9cec80223251be38409cf4205
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-10-21 07:30:59 +0900

    不具合確認

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 竹内秀樹 on 2017/10/20.
+//  Copyright © 2017年 hideki.takeuchi. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    //カテゴリー
+    dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
